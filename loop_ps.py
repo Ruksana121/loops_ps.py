7500fafbaf47b5d6d3c095fe7172fff7919403ef
Ruksana121 <153189505+Ruksana121@users.noreply.github.com>
2025-02-23 00:28:33 +0530

    Add files via upload

---
+++ loop_ps.py
@@ -0,0 +1,8 @@
+
+# # v. Reverse a number using a while loop. 1. Also can we get the sum of all the digits.num=int(input("Enter a number: "))#23456
+# rev_num=0
+# while(num>0):
+#     rem=num%10#6
+#     num=num//10#2345
+#     rev_num=rev_num*10+rem
+# print(rev_num)
